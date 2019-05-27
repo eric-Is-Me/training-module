2cedd1b9bd7cc862872af7a79efcf719a4f2a741
Eric <eric.chaing.official@gmail.com>
2019-05-26 19:34:54 -0700

    Player movement created
    Uses rigid body to enable physics so we can handle collision events later.

--- Assets/PlayerActions.cs
+++ Assets/PlayerActions.cs
@@ -4,13 +4,16 @@ using UnityEngine;
  
 public class PlayerActions : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public float speed;
+	public Rigidbody2D player;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// FixedUpdate to call updates at fixed rate independent from frame rate.
+	void FixedUpdate () {
+		// Input lets us use the arrow and WASD keys for movement
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		// Need 3 element vector since the object position has 3 parameters (x,y,z)
+		Vector3 move = new Vector3 (moveHorizontal, 0, 0);
+		player.MovePosition(player.transform.position + (move * speed))
 	}
 }
